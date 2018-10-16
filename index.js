47caec1a98b2a5d080a455088268d1337e624940
sondos alansi <sondos.alansi@yahoo.com>
2018-10-16 20:23:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function createPost(){
   let ti=document.getElementById('postTitle').value;
-   let bo=document.getElementById('postTitle').value;
-    let au=document.getElementById('postTitle').value;
+   let bo=document.getElementById('postbody').value;
+    let au=document.getElementById('author').value;
 }
 
 function postComment(){
