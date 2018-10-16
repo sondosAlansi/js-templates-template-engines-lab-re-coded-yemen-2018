07fc2c4afc798f4c73d5ba49b3c489c6b866f5a7
sondos alansi <sondos.alansi@yahoo.com>
2018-10-16 20:27:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,4 +7,17 @@ function createPost(){
 function postComment(){
   let commenter =document.getElementById('commenterName').value;
   let comment =document.getElementById('commentText').value;
+   let commentTemplate =
+    '<div class="comment"><p><%= comment %></p><p>Posted By: <span class="commenter"><%= commenter %></span></p></div>';
+ 
+  let templateFn = _.template(commentTemplate);
+ 
+  let commentsDiv = document.getElementById('comments');
+ 
+ 
+  let templateHTML = templateFn({ comment: comment, commenter: commenter });
+ 
+  //append rather than replace!
+  commentsDiv.innerHTML += templateHTML;
+}
 }
