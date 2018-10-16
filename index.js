7a05243aabfa9a5715de367d224807b3eac5c9c5
sondos alansi <sondos.alansi@yahoo.com>
2018-10-16 20:26:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function createPost(){
 }
 
 function postComment(){
-  let name=document.getElementById('commenterName').value;
+  let commenter =document.getElementById('commenterName').value;
   let comment=document.getElementById('commentText').value;
 }
